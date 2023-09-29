1ad150d5bd6c7a953ed98f4b8eba2580c11f6d51
honguyen00 <lehonguyen00@gmail.com>
2023-09-29 21:32:41 +1000

    develop feature to get the current user's location, and when dbclick on said location, show parking around set radius

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,56 +1,130 @@
 let map;
 let service;
+let infoWindow;
+let markers = [];
+
 
 function initMap() {
-    // location of Sydney
-    const initialPositon = { lat: -33.865, lng:  151.210 };
-
+    const location = {
+        au: {
+            center: { lat: -25.3, lng: 133.8 },
+            zoom: 4,
+      }}
     map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 8,
-        center: initialPositon,
-        mapId: "default_map",
+        zoom: location.au.zoom,
+        center: location.au.center,
+        mapId: "My-map-id-1",
         mapTypeControl: false,
         streetViewControl: false,
     });
 
-    const request = {
-        keyword: "parking",
-        location: initialPositon,
-        radius: 50000,
-        type: 'parking',
-    }; 
-
+    infoWindow = new google.maps.InfoWindow();
+    // const currentLocationButton = document.createElement("button");
+    // currentLocationButton.textContent = "Jump to current location";
+    // currentLocationButton.classList.add("current-butt");
+    const currentLocationButton = $("<button class='custom-map-control-button'>Jump to current location</button>")
+    map.controls[google.maps.ControlPosition.TOP_CENTER].push(currentLocationButton[0]);
+    currentLocationButton.on("click", getCurrentPos);
     service = new google.maps.places.PlacesService(map);
-    service.nearbySearch(request, (results, status) => {
-        if (status === google.maps.places.PlacesServiceStatus.OK && results) {
-            for (let i = 0; i < results.length; i++) {
-                createMarker(results[i]);
+}
+
+function getCurrentPos() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((position) => {
+            const pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude,
             }
+            createMarker(pos);
+        },
+        () => {
+            handleLocationError(true, infoWindow, map.getCenter)
         }
-        map.setCenter(results[0].geometry.location);
-        map.setZoom(15);
+        );
+    }
+    else {
+        handleLocationError(false, infoWindow, map.getCenter());
+    }
+}
+
+function searchParkingAroundRadius(position) {
+    if (typeof position == String) {
+        console.log("Is a string");
+    }
+    else {
+        var location = position.latLng.toJSON();
+        const search = {
+            location: {lat: location["lat"], lng: location["lng"]},
+            radius: 1000,
+            types: ["parking"],
+            keyword: "parking"
+        };
+        service.nearbySearch(search, (results, status, pagination) => {
+            if (status === google.maps.places.PlacesServiceStatus.OK && results) {
+                // clearResults();
+                // clearMarkers();
+                for (var i=0; i<results.length; i++) {
+                    markers[i] = new google.maps.Marker({
+                        position: results[i].geometry.location,
+                        animation: google.maps.Animation.DROP,
+                        icon: "./assets/images/icon1.png",
+                    });
+
+                    markers[i].placeResult = results[i];
+                    markers[i].addListener("click", showParkingInfo);
+                    setTimeout(dropMarker(i), i*100);
+                    // addResult(results[i], i);
+                }
+
+            }
+        })
+    }
+}
+
+
+function dropMarker(i) {
+    return function () {
+        markers[i].setMap(map);
+    }
+}
+
+function showParkingInfo() {
+    const marker = this;
+    service.getDetails({placeId: marker.placeResult.place_id}, (place, status) => {
+        if (status !== google.maps.places.PlacesServiceStatus.OK) {
+            return;
+        }
+        infoWindow.open(map, marker);
     })
 }
 
-function createMarker(place) {
-    if (!place.geometry || !place.geometry.location) return;
 
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(
+      browserHasGeolocation
+        ? "Error: The Geolocation service failed."
+        : "Error: Your browser doesn't support geolocation.",
+    );
+    infoWindow.open(map);
+  }
+
+function createMarker(place) {
+    if (!place) return;
+    map.setCenter(place);
+    map.setZoom(12)
     const marker = new google.maps.Marker({
         map: map,
-        position: place.geometry.location,
+        position: place,
+        animation: google.maps.Animation.DROP,
     });
 
-    var infowindow = new google.maps.InfoWindow(
+    infowindow = new google.maps.InfoWindow(
         {
-            content: place.name,
+            content: "{ "+ place.lat + ", " + place.lng + "}",
         }
     );
-    marker.addListener("click", () => {
-        infowindow.open({
-            anchor: marker,
-            map,
-        });
-    })
+    marker.addListener("dbclick", searchParkingAroundRadius, {passive: true})
 }
 
 window.initMap = initMap;
